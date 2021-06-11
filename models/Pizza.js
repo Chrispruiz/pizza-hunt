5280dd9d9a639647ab38d9572c2dddfdfe290c23
Chris Ruiz <chrispruiz@att.net>
2021-06-10 22:23:46 -0500

    added validation features

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,19 +3,25 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-      type: String
+        type: String,
+        required: 'Please provide a pizza name!',
+        trim: true
     },
     createdBy: {
-      type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
         type: Date,
         default: Date.now,
         get: (createdAtVal) => dateFormat(createdAtVal)
-      },
+    },
     size: {
-      type: String,
-      default: 'Large'
+        type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+        default: 'Large'
     },
     toppings: [],
     comments: [
