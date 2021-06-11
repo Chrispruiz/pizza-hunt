1a96ef2c87fc053503bdc391c481937fa06167db
Chris Ruiz <chrispruiz@att.net>
2021-06-10 22:04:21 -0500

    added db funcctionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+//pulls in from comment-controller.js file
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
 
+// delete replies
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
